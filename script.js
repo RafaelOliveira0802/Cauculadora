3c89745a362dc00ed3f8c53dd115955ff8646323
Rafael Oliveira <128871938+RafaelOliveira0802@users.noreply.github.com>
2024-01-03 22:51:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function somar() {
+    var n1 = parseInt(document.getElementById("num1").value);
+    var n2 = parseInt(document.getElementById("num2").value);
+    var resultado = n1 + n2
+
+    document.getElementById("result").innerHTML=resultado;
+
+    console.log(n1);
+    console.log(n2);
+    console.log(resultado);
+}
+
+function subtrair() {
+    var n1 = parseInt(document.getElementById("num1").value);
+    var n2 = parseInt(document.getElementById("num2").value);
+    var resultado = n1 - n2
+
+    document.getElementById("result").innerHTML=resultado;
+
+    console.log(n1);
+    console.log(n2);
+    console.log(resultado);
+}
+
+function multiplicar() {
+    var n1 = parseInt(document.getElementById("num1").value);
+    var n2 = parseInt(document.getElementById("num2").value);
+    var resultado = n1 * n2
+
+    document.getElementById("result").innerHTML=resultado;
+
+    console.log(n1);
+    console.log(n2);
+    console.log(resultado);
+}   
+
+function dividir() {
+    var n1 = parseInt(document.getElementById("num1").value);
+    var n2 = parseInt(document.getElementById("num2").value);
+    var resultado = n1 / n2
+
+    document.getElementById("result").innerHTML=resultado;
+
+    console.log(n1);
+    console.log(n2);
+    console.log(resultado);
+}  
